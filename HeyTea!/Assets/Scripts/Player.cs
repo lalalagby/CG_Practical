73f7b38868541d6cd4455f28b0a7e33208111bda
YongWu <2437557521@qq.com>
2023-08-27 14:20:40 +0200

    restructure move manage and load mode
    1.Decouple movement, model and character code.
    2.load the moving animation of the character, and the camera.

--- HeyTea!/Assets/Scripts/Player.cs
+++ HeyTea!/Assets/Scripts/Player.cs
@@ -15,28 +15,13 @@ public class Player : MonoBehaviour
 {
     //define the player's move speed
     [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private GameInput gameInput;
+
+    private bool isWalking;
 
     //callback each update
     private void Update() {
-        //input vector,x=x axis move,y=y axis move
-        Vector2 inputVector = new Vector2(0, 0);
-
-        //get move input
-        if (Input.GetKey(KeyCode.W)) {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A)) {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            inputVector.x = +1;
-        }
-
-        //move normalize to 1
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.GetMoveVector();
 
         //transfer to game coordinate
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
@@ -44,12 +29,16 @@ public class Player : MonoBehaviour
         //update player postion
         transform.position += moveDir* moveSpeed*Time.deltaTime;
 
+        isWalking = moveDir != Vector3.zero;
+
         //update dirction
         float rotatedSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward , moveDir, rotatedSpeed*Time.deltaTime);
 
-        Debug.Log(inputVector);
+    }
 
+    public bool IsWalking() {
+        return isWalking;
     }
 
 }
