446a359a77188f2fac64fb18b6716b4a002ba320
Xinyue Cheng <13523092590@163.com>
2024-06-30 17:01:48 +0200

    Update Delivery Manger(Generate Order)

---
+++ HeyTea!/Assets/Scripts/ScriptableObject/RecipeListSO.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * @brief ScriptableObject for managing a list of recipes in the HeyTea game.
+ * 
+ * @details This ScriptableObject holds a list of RecipeSO objects, allowing for easy management
+ * and access to multiple recipes within the game. Each RecipeSO in the list represents a different
+ * recipe that can be utilized in the HeyTea game.
+ * 
+ * @date 30.06.2024
+ * @author Xinyue Cheng
+ */
+public class RecipeListSO : ScriptableObject
+{
+    /**
+     * @brief List of RecipeSO objects.
+     * 
+     * @details This list contains multiple RecipeSO objects, each representing a different recipe
+     * in the HeyTea game. It allows for centralized management of all recipes.
+     */
+    public List<RecipeSO> recipeSOList;
+}
+
---
+++ HeyTea!/Assets/Scripts/ScriptableObject/RecipeSO.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * @brief ScriptableObject for defining recipes in the HeyTea game.
+ * 
+ * @details This ScriptableObject holds a list of HeyTeaObjectSO objects that are part of the recipe,
+ * as well as the name of the recipe. It is used to define the ingredients and the name for various
+ * recipes in the HeyTea game.
+ * 
+ * @date 30.06.2024
+ * @author Xinyue Cheng
+ */
+[CreateAssetMenu()]
+public class RecipeSO : ScriptableObject
+{
+    /**
+     * @brief List of HeyTeaObjectSO objects that make up the recipe.
+     * 
+     * @details This list contains the ingredients required for the recipe. Each element in the list
+     * is a HeyTeaObjectSO that represents an ingredient.
+     */
+    public List<HeyTeaObjectSO> heyTeaObjectSOLists;
+
+    /**
+     * @brief The name of the recipe.
+     * 
+     * @details This string holds the name of the recipe. It can be used to identify the recipe in the game.
+     */
+    public string recipeName;
+}
