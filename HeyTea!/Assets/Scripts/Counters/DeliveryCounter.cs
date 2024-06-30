c5c16b184c9ed7f3a70039f98321535d98999b65
Xinyue Cheng <13523092590@163.com>
2024-06-30 15:39:23 +0200

    Update DeliveryCounter

---
+++ HeyTea!/Assets/Scripts/Counters/DeliveryCounter.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        if (player.GetHeyTeaObject() is CupObject)
+        {
+            player.GetHeyTeaObject().DestroySelf();
+        }
+    }
+}
